cb4b169e61ac7a0b8b019b56e9b50af7a3b8ecdf
Liaoyyy <2109646834@qq.com>
2023-10-21 00:34:02 +0800

    start ch3

--- code/Proj1_Q_learning_maze/main.py
+++ code/Proj1_Q_learning_maze/main.py
@@ -5,8 +5,8 @@ import Q_learning as ql
 import time
 
 # Create a maze num '1' stands for a block and num'-1' refers to the destination
-maze = np.array([[0, 0, 0, 1], [1, 1, 0, 1], [0, 0, 0, 0], [1, 0, 1, -1]])
-
+#maze = np.array([[0, 0, 0, 1], [1, 1, 0, 1], [0, 0, 0, 0], [1, 0, 1, -1]])
+maze = np.array([[0, 0, 1, 0, 1], [1, 0, 0, 0, 0], [0, 0, 1, 1, 0], [1, 0, 1, 0,-1]])
 Actions = ['up', 'down', 'left', 'right']
 epoch = 80
 row, col = maze.shape
