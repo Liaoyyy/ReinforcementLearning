87d1af2f60106f11f7eb2ea2bc4fd932ffe0b18c
Your Name <you@berkeley.edu>
2023-10-26 08:27:49 +0800

    modify eg4.3

--- code/ch4/eg4.3.py
+++ code/ch4/eg4.3.py
@@ -3,15 +3,17 @@ import numpy as np
 from matplotlib import pyplot as plt
 import time
 
-state=list(range(0,101,1))#状态空间
+target=10
+state=list(range(0,target+1,1))#状态空间
 p_h=0.25 #抛硬币正面朝上概率
-stateValues=np.random.random(size=101) #状态价值函数
+stateValues=np.random.random(size=target+1) #状态价值函数
 stateValues[0]=0
-stateValues[100]=1
+stateValues[target]=1
 bound = 0.01
+gamma = 0.9
 
 def initStrategy(step):
-    strategy=[[0]]*(step-1)
+    strategy=[[0]]*(step)
     for i in range(1,step-1):
         strategy[i]=[np.random.randint(0,min(i,step-i-1))]
     return strategy
@@ -23,21 +25,36 @@ def generateRandomNum(p_h):
     else:
         return 0#背面朝上
 
+#单步策略改进
+def searchBestAction(stateValues,curState):
+    newStepStrat=[]#新策略
+    allowedAction=list(range(0,min()))
+    return newStepStrat
+
 #使用策略迭代方法
-def DP(bound):
-    #策略评估
-    delta=1
-    while(delta>bound):
-        delta=0
+def DP(bound,stateValues,strategy):
+
+    while(1):
+        #策略评估
+        delta=1
+        while(delta>bound):
+            delta=0.0
+            for s in state:
+                v=stateValues[s]
+                action=np.random.choice(strategy[s]) #根据策略函数做出选择
+                stateValues[s]=p_h*(action+gamma*stateValues[s+action])+(1-p_h)*(-action+gamma*stateValues[s-action])
+                delta=max(delta,abs(v-stateValues[s]))
+
+        #策略改进
+        policyStable=True
         for s in state:
-            v=stateValues[s]
-            action=np.random.choice(strategy[s]) #根据策略函数做出选择
-            stateValues[s]=p_h*(action+v)+(1-p_h)*(v)
-            delta=max(delta,abs(v-stateValues[s]))
+            oldAction=strategy[s]
+
+
 
 if __name__ == "__main__":
-    strategy=initStrategy(101) #随机初始化策略函数
+    strategy=initStrategy(target+1) #随机初始化策略函数
     startTime=time.time()
-    DP(bound)
+    DP(bound,stateValues,strategy)
     endTime=time.time()
-    print("Traning period="+(startTime-endTime))
+    print("Traning period=",(endTime-startTime))
